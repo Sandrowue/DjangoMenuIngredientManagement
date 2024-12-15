6bffa144516823e16a1019323211661e288d76e2
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-12-15 13:38:52 +0200

    Projektin sisäinen appi alustettu

---
+++ capstone/MenuIngredientManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MenuingredientmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MenuIngredientManagement'
